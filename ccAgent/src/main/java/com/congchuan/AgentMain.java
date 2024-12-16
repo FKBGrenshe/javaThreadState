78b6829e9219297a67406824215685d81152a142
FKBG人设 <fkbgrenshe@163.com>
2024-12-16 21:03:14 +0800

    ver1.1：新增堆内存快照

--- ccAgent/src/main/java/com/congchuan/AgentMain.java
+++ ccAgent/src/main/java/com/congchuan/AgentMain.java
@@ -2,6 +2,7 @@ package com.congchuan;
 
 import com.congchuan.command.MemoryCommand;
 
+import java.io.IOException;
 import java.lang.instrument.Instrumentation;
 
 public class AgentMain {
@@ -10,8 +11,12 @@ public class AgentMain {
         System.out.println("premain doing...");
     }
 
-    public static void agentmain(String agentArgs, Instrumentation inst){
+    public static void agentmain(String agentArgs, Instrumentation inst) throws IOException {
         System.out.println("agentmain doing...");
+        System.out.println("打印内存状态ing");
         MemoryCommand.printMemory();
+
+        System.out.println("堆内存快照");
+        MemoryCommand.heapDump();
     }
 }
--- ccAgent/src/main/java/com/congchuan/command/MemoryCommand.java
+++ ccAgent/src/main/java/com/congchuan/command/MemoryCommand.java
@@ -1,9 +1,14 @@
 package com.congchuan.command;
 
+import com.sun.management.HotSpotDiagnosticMXBean;
+
+import java.io.IOException;
 import java.lang.management.BufferPoolMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.MemoryType;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MemoryCommand {
@@ -27,14 +32,14 @@ public class MemoryCommand {
             for (BufferPoolMXBean bufferPoolMXBean : bufferPoolMXBeans) {
                 StringBuilder stringBuilder = new StringBuilder();
                 stringBuilder
-                        .append(" name:")
-                        .append(bufferPoolMXBean.getName())
+                        .append(" name:\t")
+                        .append(bufferPoolMXBean.getName()+"\t")
                         .append(bufferPoolMXBean.getMemoryUsed()/1024/1024)
-                        .append("m")
+                        .append("m"+"\t")
                         //
-                        .append(" capacity:")
+                        .append(" capacity:"+"\t")
                         .append(bufferPoolMXBean.getTotalCapacity()/1024/1024)
-                        .append("m");
+                        .append("m"+"\t");
                 System.out.println(stringBuilder);
             }
 
@@ -51,21 +56,37 @@ public class MemoryCommand {
                 .forEach(x ->{
                     StringBuilder stringBuilder = new StringBuilder();
                     stringBuilder
-                            .append(" name:")
-                            .append(x.getName())
+                            .append(" name:"+"\t")
+                            .append(x.getName()+"\t")
                             .append(x.getUsage().getUsed()/1024/1024)
-                            .append("m")
+                            .append("m"+"\t")
                             //
-                            .append(" committed:")
-                            .append(x.getName())
+                            .append(" committed:"+"\t")
+                            .append(x.getName()+"\t")
                             .append(x.getUsage().getCommitted()/1024/1024)
-                            .append("m")
+                            .append("m"+"\t")
                             //
-                            .append(" max:")
-                            .append(x.getName())
+                            .append(" max:"+"\t")
+                            .append(x.getName()+"\t")
                             .append(x.getUsage().getMax()/1024/1024)
-                            .append("m");
+                            .append("m"+"\t");
                     System.out.println(stringBuilder);
                 });
     }
+
+    // 生成内存快照
+    public static void heapDump() throws IOException {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm");
+
+        HotSpotDiagnosticMXBean hotSpotDiagnosticMXBean = ManagementFactory.getPlatformMXBean(HotSpotDiagnosticMXBean.class);
+        //
+        String outputFile = simpleDateFormat.format(new Date()) + ".hprof";
+        hotSpotDiagnosticMXBean.dumpHeap(outputFile,true);
+        System.out.println("\theapDump( "+outputFile+" , true) -- 只保存存活对象\t\n");
+
+
+    }
+
+
 }
